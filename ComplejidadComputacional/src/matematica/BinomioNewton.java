bc4360e1943cda96e7dab7f5b86c57ae3cad1a6d
MaxiLima <maxi.lima94@gmail.com>
2017-09-13 17:43:29 -0300

    Clases principales

---
+++ ComplejidadComputacional/src/matematica/BinomioNewton.java
@@ -0,0 +1,109 @@
+package matematica;
+
+public class BinomioNewton {
+
+	private int grado;
+	private double x;
+	private double b;
+	
+	public BinomioNewton(int grado, double x, double b) {
+		this.grado = grado;
+		this.x = x;
+		this.b = b;
+	}
+	public BinomioNewton(int grado) {
+		this.grado = grado;
+		this.x = this.b = 0;
+	}
+	
+	public BinomioNewton() {
+		this(0,0,0);
+	}
+	
+	public int getGrado() {
+		return grado;
+	}
+	public void setGrado(int grado) {
+		this.grado = grado;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double coeficienteA) {
+		this.x = coeficienteA;
+	}
+	public double getB() {
+		return b;
+	}
+	public void setB(double coeficienteB) {
+		this.b = coeficienteB;
+	}
+
+	
+	public static int[] obtenerTerminosTarta(int nivel) {
+		
+		int[] resultado = new int[nivel];
+		int[][] tartaglia = new int[nivel][nivel];
+		
+	    for(int i = 0 ; i < nivel; i++)
+	    {
+	        tartaglia[i][0] = 1;
+	        for(int j= 1 ; j  <= i ; j++)
+	                tartaglia[i][j]  = tartaglia[i-1][j-1]+ tartaglia[i-1][j];
+	    }
+		for (int i = 0; i < tartaglia[nivel -1].length; i++) {
+			resultado[i] = tartaglia[nivel -1][i];
+		}
+		return resultado;
+			
+	}
+	
+	public String mostrarBinomioResuelto() {
+		
+		StringBuffer sb = new StringBuffer();
+		int [] terminos = obtenerTerminosTarta(this.getGrado() +1);
+		double temporal;
+		for(int i = 0, j = this.getGrado(); i <= this.getGrado() ; j--,i ++) {
+			
+			temporal = terminos[i]*Math.pow(this.getX(), j)*Math.pow(this.getB(),i);
+			if(temporal > 0)
+			sb.append("+");
+			sb.append(temporal);
+			sb.append("X");
+			sb.append(j);
+			
+		}
+		return sb.toString();
+	}
+	
+	public  double resolverBinomio() {
+		
+		double resultado = 0;
+		int[] terminos = new int[this.getGrado()];
+		terminos = obtenerTerminosTarta(this.getGrado()+1);
+		for (int i = this.getGrado(), j =0; i >= 0; i--, j++) {
+			resultado += terminos[j]* Math.pow(this.x, i) * Math.pow(this.b, j);
+		}
+		
+
+		return resultado;
+	}
+	
+	//METODO DE CLASE
+	public static double resolverBinomio(double x, double b, int grado,double valor) {
+		
+		double retorno = 0;
+		int[] terminos = new int[grado];
+		
+		terminos = obtenerTerminosTarta(grado+1);
+		for (int i = grado, j =0; i >= 0; i--, j++) 
+			retorno += terminos[j] * Math.pow(valor*x, i) * Math.pow(b, j);
+		
+		
+
+		return retorno;
+	}
+	
+	
+	
+}
---
+++ ComplejidadComputacional/src/matematica/Polinomio.java
@@ -0,0 +1,145 @@
+package matematica;
+
+
+public class Polinomio {
+	
+	
+	private int grado;
+	private double[] coeficientes;
+	
+	public Polinomio(int grado){
+		
+		double[] coeficientes = new double[grado];
+		for (int i = 0; i < grado; i++) 
+			coeficientes[i] = 1;
+		this.setCoeficientes(coeficientes);
+	}
+	
+	public Polinomio(int grado, double[] coeficientes) {
+		this.grado = grado;
+		this.coeficientes = coeficientes;
+	}
+
+	public int getGrado() {
+		return grado;
+	}
+	
+	public void setGrado(int grado) {
+		this.grado = grado;
+	}
+	
+	public double[] getCoeficientes() {
+		return coeficientes;
+	}
+	
+	public void setCoeficientes(double[] coeficientes) {
+		this.coeficientes = coeficientes;
+	}
+
+	
+	public double evaluarProgDinamica(double x) {
+		
+		double resultado = 0;
+		double pot = 1;
+		
+		for (int i = this.getGrado(); i >= 0; i--) {
+			resultado += this.getCoeficientes()[i]*pot;
+			pot *= x; //O(1)
+		}//O(n)
+		
+		return resultado;
+	}
+	
+	
+	public double evaluarMSucesivas(double x) {
+		
+		double resultado = 0;
+		
+		for (int i = 0; i <= this.getGrado(); i++) 
+			resultado += this.getCoeficientes()[i]*potenciaPorMult(x, this.getGrado() - i );//O(n2)
+		
+		return resultado;
+	}
+	
+	public double evaluarRecursiva(double x) {
+		
+		double resultado = 0;
+		
+		for (int i = 0; i <= this.getGrado(); i++) 
+			resultado += this.getCoeficientes()[i]*potenciaRecursiva(x, this.getGrado() - i );
+		
+		return resultado;
+	}
+	
+	public double evaluarHorner(double x) {
+		double resultado = 0;
+		
+		resultado = this.getCoeficientes()[0]*x + this.getCoeficientes()[1];
+		
+		for (int i = 2; i <= this.getGrado(); i++) 
+			resultado = (resultado*x) + this.getCoeficientes()[i];
+		
+
+		return resultado;
+	}
+	
+	
+	public double evaluarRecursivaPar(double x){
+		
+		double resultado = 0;
+		
+		for (int i = 0; i <= this.getGrado(); i++) 
+		{
+			if(i % 2 != 0)
+				resultado += this.getCoeficientes()[i]*potenciaRecursiva(x, this.getGrado() - i );//Impar
+			else
+				resultado += this.getCoeficientes()[i]*potenciaRecursivaPar(x, this.getGrado() - i );//Par
+		}
+			
+		
+		return resultado;
+	}
+	
+	
+	public double evaluarPow(double x){
+		
+		double resultado = 0;
+		for(int i = 0; i <= this.getGrado(); i ++)
+			resultado += this.getCoeficientes()[i]* Math.pow(x,this.getGrado()-i);
+			
+		return resultado;
+	}
+	public double potenciaPorMult(double numero, int pot){
+		
+		double resultado = 1;
+		
+		for (int i = pot; i > 0; i--) 
+			resultado *= numero;
+			
+		
+		return resultado;
+	} 
+	
+	public double potenciaRecursiva(double numero, int pot) {
+		
+		if(pot == 0)
+			return 1;
+		else
+			return numero*potenciaRecursiva(numero, pot -1);
+	}
+	
+	public double potenciaRecursivaPar(double numero, int pot) {
+		
+		if(pot == 0)
+			return 1;
+		else
+			return numero*potenciaRecursiva(numero*numero, pot /2);
+	}
+	
+
+	
+	
+	
+	
+	
+}
