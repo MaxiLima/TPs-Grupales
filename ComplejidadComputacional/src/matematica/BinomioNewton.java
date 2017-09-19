1d2e368dd2f54d28de80e754830486d11ed9be53
MaxiLima <maxi.lima94@gmail.com>
2017-09-19 17:01:16 -0300

    commit Final

--- ComplejidadComputacional/src/matematica/BinomioNewton.java
+++ ComplejidadComputacional/src/matematica/BinomioNewton.java
@@ -1,107 +1,192 @@
 package matematica;
 
 public class BinomioNewton {
-
 	private int grado;
 	private double x;
 	private double b;
+	private double[][] tarta;
 	
+
 	public BinomioNewton(int grado, double x, double b) {
+		super();
 		this.grado = grado;
 		this.x = x;
 		this.b = b;
+		this.tarta = new double[grado + 1][grado + 1];
 	}
+	
 	public BinomioNewton(int grado) {
+		super();
 		this.grado = grado;
 		this.x = this.b = 0;
+		this.tarta = new double[grado + 1][grado + 1];
 	}
-	
+
 	public BinomioNewton() {
-		this(0,0,0);
+		this(0, 0, 0);
+	}
+
+	public double evaluarMSucesivas(double x) {
+		double retorno = 0;
+		retorno = potenciaPorMult((this.getX() * x) + this.getB(),this.getGrado());
+		return retorno;
+	}
+
+	public double evaluarRecursiva(double x) {
+		double retorno = 0;
+		for (int i = 0; i <= this.getGrado(); i++)
+			retorno = potenciaRecursiva((this.getX() * x) + this.getB(),this.getGrado());
+		return retorno;
 	}
 	
+	public double evaluarRecursivaPar(double x) {
+		double retorno = 0;
+		for (int i = 0; i <= this.getGrado(); i++) {
+			if (i % 2 != 0)
+				retorno = potenciaRecursiva((this.getX() * x) + this.getB(),this.getGrado());
+			else
+				retorno = potenciaRecursivaPar((this.getX() * x) + this.getB(),this.getGrado());
+		} 
+
+		return retorno;
+	}
+	
+	public double evaluarProgDinamica(double x) {
+		double retorno = this.getX()*x + this.getB();
+		double potencia = retorno;
+		for (int i = 1; i < this.getGrado(); i++) 
+		    retorno*= potencia;
+		return retorno;
+	}
+	
+	public double evaluarPow(double x) {
+		double retorno = 0;
+		retorno = Math.pow((this.getX() * x) + this.getB(), this.getGrado());
+		return retorno;
+	}
+	
+	public double evaluarHorner(double x) {
+		double[] terminos = obtenerTerminosTarta(this.getGrado() + 1);
+		double[] coeficientes = new double[this.getGrado() + 1];
+		
+		for (int i = 0, j = this.getGrado(); i <= this.getGrado(); j--, i++) {
+			coeficientes[i] = terminos[i] * Math.pow(this.getX(), j) * Math.pow(this.getB(), i);
+		}		
+		double retorno = 0;
+		if (this.getGrado() == 0)
+			return coeficientes[0];
+		retorno = coeficientes[0] * x + coeficientes[1];
+		for (int i = 2; i <= this.getGrado(); i++)
+			retorno = (retorno * x) + coeficientes[i];
+		return retorno;
+	}
+
+	public double[] obtenerTerminosTarta(int nivel) {
+		
+		if (tarta[nivel - 1][0] != 0)
+			return tarta[nivel - 1];
+		for (int i = 0; i < nivel; i++) {
+			if (tarta[i][0] == 0) {
+				tarta[i][0] = 1;
+				for (int j = 1; j <= i; j++)
+					tarta[i][j] = Double.valueOf(tarta[i - 1][j - 1]
+							+ tarta[i - 1][j]);
+			}
+		}
+		return tarta[nivel - 1];
+	}
+
+	public String mostrarBinomioResuelto() {
+		StringBuffer buffer = new StringBuffer();
+		double[] terminos = obtenerTerminosTarta(this.getGrado() + 1);
+		double temporal;
+		for (int i = 0, j = this.getGrado(); i <= this.getGrado(); j--, i++) {
+			temporal = terminos[i] * Math.pow(this.getX(), j) * Math.pow(this.getB(), i);
+			if (temporal > 0)
+				buffer.append("+");
+			buffer.append(temporal);
+			buffer.append("X");
+			buffer.append(j);
+		}
+		return buffer.toString();
+	}
+
+	public double resolverBinomio() {
+		double retorno = 0;
+		double[] terminos = new double[this.getGrado() + 1];
+		terminos = obtenerTerminosTarta(this.getGrado() + 1);
+		for (int i = this.getGrado(), j = 0; i >= 0; i--, j++) {
+			retorno += terminos[j] * Math.pow(this.x, i) * Math.pow(this.b, j);
+		}
+		return retorno;
+	}
+
+	public double resolverBinomio(final double x, final double b, final int grado, double valor) {
+		double retorno = 0;
+		double[] terminos = new double[grado];
+		double auxX = Math.pow(x, grado);
+		double auxB = 1;
+		terminos = obtenerTerminosTarta(grado + 1);
+
+		for (int i = grado, j = 1; i >= 0; i--, j++) {
+			retorno += terminos[j - 1] * (valor * auxX) * auxB;
+			auxX /= x;
+			auxB *= b;
+		}
+		return retorno;
+	}
+
+	public double potenciaPorMult(double numero, int potencia) {
+		double retorno = 1;
+		for (int i = potencia; i > 0; i--)
+			retorno *= numero;
+		return retorno;
+	}
+
+	public double potenciaRecursiva(double numero, int potencia) {
+		if (potencia == 0)
+			return 1;
+		else
+			return numero * potenciaRecursiva(numero, potencia - 1);
+	}
+
+	public double potenciaRecursivaPar(double numero, int potencia) {
+		if (potencia == 0)
+			return 1;
+		if ((potencia % 2) == 0)
+			return potenciaRecursivaPar(numero * numero, potencia / 2); 
+		return numero * potenciaRecursivaPar(numero, potencia - 1);
+	}
+
 	public int getGrado() {
 		return grado;
 	}
+
 	public void setGrado(int grado) {
 		this.grado = grado;
 	}
+
 	public double getX() {
 		return x;
 	}
+
 	public void setX(double coeficienteA) {
 		this.x = coeficienteA;
 	}
+
 	public double getB() {
 		return b;
 	}
+
 	public void setB(double coeficienteB) {
 		this.b = coeficienteB;
 	}
 
-	
-	public static long[] obtenerTerminosTarta(int nivel) {
-		
-		long[] resultado = new long[nivel];
-		long[][] tartaglia = new long[nivel][nivel];
-		
-	    for(int i = 0 ; i < nivel; i++)
-	    {
-	        tartaglia[i][0] = 1;
-	        for(int j= 1 ; j  <= i ; j++)
-	                tartaglia[i][j]  = tartaglia[i-1][j-1]+ tartaglia[i-1][j];
-	    }
-		for (int i = 0; i < tartaglia[nivel -1].length; i++) {
-			resultado[i] = tartaglia[nivel -1][i];
-		}
-		return resultado;
-			
+	public double[][] tarta() {
+		return tarta;
 	}
-	
-	public String mostrarBinomioResuelto() {
-		
-		StringBuffer buffer = new StringBuffer();
-		long[] terminos = obtenerTerminosTarta(this.getGrado() +1);
-		double aux;
-		for(int i = 0, j = this.getGrado(); i <= this.getGrado() ; j--,i ++) {
-			
-			aux = terminos[i]*Math.pow(this.getX(), j)*Math.pow(this.getB(),i);
-			if(aux > 0)
-			buffer.append("+");
-			buffer.append(aux);
-			buffer.append("X");
-			buffer.append(j);
-			
-		}
-		return buffer.toString();
+
+	public void setTartaglia(double[][] tarta) {
+		this.tarta = tarta;
 	}
-	
-	public  double resolverBinomio() {
-		
-		double resultado = 0;
-		long[] terminos = new long[this.getGrado()];
-		terminos = obtenerTerminosTarta(this.getGrado()+1);
-		for (int i = this.getGrado(), j = 0; i >= 0; i--, j++) {
-			resultado += terminos[j]* Math.pow(this.x, i) * Math.pow(this.b, j);
-		}
-		
-
-		return resultado;
-	}
-	
-
-	public static double resolverBinomio(double x, double b, int grado,double valor) {
-		
-		double resultado = 0;
-		long[] terminos = new long[grado];
-		
-		terminos = obtenerTerminosTarta(grado+1);
-		for (int i = grado, j =0; i >= 0; i--, j++) 
-			resultado += terminos[j] * Math.pow(valor*x, i) * Math.pow(b, j);
-		
-		
-
-		return resultado;
-	}	
-	
 }
--- ComplejidadComputacional/src/test/matematica/BinomioTest.java
+++ ComplejidadComputacional/src/test/matematica/BinomioTest.java
@@ -20,46 +20,46 @@ public class BinomioTest {
 		binomio3 = new BinomioNewton(8000,557,87);
 	}
 
-	@Test
-	public void obtenerTerminosTartaTest() {
-	
-		BinomioNewton binom30 = new BinomioNewton(30,4,4);
-		long[]resultado = new long[binom30.getGrado()];
-		long[]esperado = {1,30,435,4060,27405,142506,593775,2035800,5852925,14307150,30045015,54627300,86493225,119759850,145422675,155117520,145422675,119759850,86493225,54627300,30045015,14307150,5852925,2035800,593775,142506,27405,4060,435,30,1};
-		resultado = BinomioNewton.obtenerTerminosTarta(binom30.getGrado() + 1);
-		long start = System.nanoTime();
-		
-		Assert.assertArrayEquals(esperado, resultado);
-		
-		long stop30 = System.nanoTime();
-		long tiempo = stop30 - start;
-		System.out.println("[Binomio]obtenerTerminosTartaTest: " + tiempo + " ns para grado 30");
-		/////////////////////////////////////////////////////////////////////////////////////////////
-		BinomioNewton binom20 = new BinomioNewton(20,4,4);
-		long []esperado2 = {1,20,190,1140,4845,15504,38760,77520,125970,167960,184756,167960,125970,77520,38760,15504,4845,1140,190,20,1};
-		resultado = BinomioNewton.obtenerTerminosTarta(binom20.getGrado() + 1);
-		long start20 = System.nanoTime();
-		
-		Assert.assertArrayEquals(esperado2, resultado);
-		
-		long stop20 = System.nanoTime();
-		long tiempo20 = stop20 - start20;
-		System.out.println("[Binomio]obtenerTerminosTartaTest: " + tiempo20 + " ns para grado 20");
-		/////////////////////////////////////////////////////////////////////////////////////////////	
-		BinomioNewton binom10 = new BinomioNewton(10,4,4);
-		long[]esperado1 = {1,10,45,120,210,252,210,120,45,10,1};
-		resultado = BinomioNewton.obtenerTerminosTarta(binom10.getGrado()+1);
-		long start10 = System.nanoTime();
-		
-		Assert.assertArrayEquals(esperado1, resultado);
-		
-		long stop10 = System.nanoTime();
-		long tiempo10 = stop10 - start10;
-		System.out.println("[Binomio]obtenerTerminosTartaTest: " + tiempo10 + " ns para grado 10");
-		
-		System.out.println();
-	}
-	
+//	@Test
+//	public void obtenerTerminosTartaTest() {
+//	
+//		BinomioNewton binom30 = new BinomioNewton(30,4,4);
+//		long[]resultado = new long[binom30.getGrado()];
+//		long[]esperado = {1,30,435,4060,27405,142506,593775,2035800,5852925,14307150,30045015,54627300,86493225,119759850,145422675,155117520,145422675,119759850,86493225,54627300,30045015,14307150,5852925,2035800,593775,142506,27405,4060,435,30,1};
+//		resultado = BinomioNewton.obtenerTerminosTarta(binom30.getGrado() + 1);
+//		long start = System.nanoTime();
+//		
+//		Assert.assertArrayEquals(esperado, resultado);
+//		
+//		long stop30 = System.nanoTime();
+//		long tiempo = stop30 - start;
+//		System.out.println("[Binomio]obtenerTerminosTartaTest: " + tiempo + " ns para grado 30");
+//		/////////////////////////////////////////////////////////////////////////////////////////////
+//		BinomioNewton binom20 = new BinomioNewton(20,4,4);
+//		long []esperado2 = {1,20,190,1140,4845,15504,38760,77520,125970,167960,184756,167960,125970,77520,38760,15504,4845,1140,190,20,1};
+//		resultado = BinomioNewton.obtenerTerminosTarta(binom20.getGrado() + 1);
+//		long start20 = System.nanoTime();
+//		
+//		Assert.assertArrayEquals(esperado2, resultado);
+//		
+//		long stop20 = System.nanoTime();
+//		long tiempo20 = stop20 - start20;
+//		System.out.println("[Binomio]obtenerTerminosTartaTest: " + tiempo20 + " ns para grado 20");
+//		/////////////////////////////////////////////////////////////////////////////////////////////	
+//		BinomioNewton binom10 = new BinomioNewton(10,4,4);
+//		long[]esperado1 = {1,10,45,120,210,252,210,120,45,10,1};
+//		resultado = BinomioNewton.obtenerTerminosTarta(binom10.getGrado()+1);
+//		long start10 = System.nanoTime();
+//		
+//		Assert.assertArrayEquals(esperado1, resultado);
+//		
+//		long stop10 = System.nanoTime();
+//		long tiempo10 = stop10 - start10;
+//		System.out.println("[Binomio]obtenerTerminosTartaTest: " + tiempo10 + " ns para grado 10");
+//		
+//		System.out.println();
+//	}
+//	
 	
 	@Test
 	public void resolverBinomioTest() {
@@ -98,38 +98,38 @@ public class BinomioTest {
 		System.out.println();
 	}
 	
-	@Test
-	public void resolverBinomioEstaticoTest() {
-		
-		long start = System.currentTimeMillis();
-		Assert.assertTrue(BinomioNewton.resolverBinomio(456, 3151, 100,2156) !=  0);
-		
-		long stop = System.currentTimeMillis();
-		long tiempo = stop - start;
-		System.out.println("[Binomio]resolverBinomioEstaticoTest: " + tiempo + " ms para grado 100");
-		/////////////////////////////////////////////////////////////////////////////////
-		start = System.currentTimeMillis();
-		Assert.assertTrue(BinomioNewton.resolverBinomio(456, 3151, 1000,2156) !=  0);
-		
-		stop = System.currentTimeMillis();
-		tiempo = stop - start;
-		System.out.println("[Binomio]resolverBinomioEstaticoTest: " + tiempo + " ms para grado 1.000" );
-		/////////////////////////////////////////////////////////////////////////////////
-		start = System.currentTimeMillis();
-		Assert.assertTrue(BinomioNewton.resolverBinomio(456, 3151, 2500,2156) !=  0);
-		
-		stop = System.currentTimeMillis();
-		tiempo = stop - start;
-		System.out.println("[Binomio]resolverBinomioEstaticoTest: " + tiempo + " ms para grado 2.500");
-		/////////////////////////////////////////////////////////////////////////////////
-		start = System.currentTimeMillis();
-		Assert.assertTrue(BinomioNewton.resolverBinomio(456, 3151, 5000,2156) !=  0);
-		
-		stop = System.currentTimeMillis();
-		tiempo = stop - start;
-		System.out.println("[Binomio]resolverBinomioEstaticoTest: " + tiempo + " ms para grado 5.000");
-		
-	}
+//	@Test
+//	public void resolverBinomioEstaticoTest() {
+//		
+//		long start = System.currentTimeMillis();
+//		Assert.assertTrue(BinomioNewton.resolverBinomio(456, 3151, 100,2156) !=  0);
+//		
+//		long stop = System.currentTimeMillis();
+//		long tiempo = stop - start;
+//		System.out.println("[Binomio]resolverBinomioEstaticoTest: " + tiempo + " ms para grado 100");
+//		/////////////////////////////////////////////////////////////////////////////////
+//		start = System.currentTimeMillis();
+//		Assert.assertTrue(BinomioNewton.resolverBinomio(456, 3151, 1000,2156) !=  0);
+//		
+//		stop = System.currentTimeMillis();
+//		tiempo = stop - start;
+//		System.out.println("[Binomio]resolverBinomioEstaticoTest: " + tiempo + " ms para grado 1.000" );
+//		/////////////////////////////////////////////////////////////////////////////////
+//		start = System.currentTimeMillis();
+//		Assert.assertTrue(BinomioNewton.resolverBinomio(456, 3151, 2500,2156) !=  0);
+//		
+//		stop = System.currentTimeMillis();
+//		tiempo = stop - start;
+//		System.out.println("[Binomio]resolverBinomioEstaticoTest: " + tiempo + " ms para grado 2.500");
+//		/////////////////////////////////////////////////////////////////////////////////
+//		start = System.currentTimeMillis();
+//		Assert.assertTrue(BinomioNewton.resolverBinomio(456, 3151, 5000,2156) !=  0);
+//		
+//		stop = System.currentTimeMillis();
+//		tiempo = stop - start;
+//		System.out.println("[Binomio]resolverBinomioEstaticoTest: " + tiempo + " ms para grado 5.000");
+//		
+//	}
 	
 	@Test
 	public void mostrarBinomioTest() {
@@ -142,4 +142,175 @@ public class BinomioTest {
 		long tiempo = stop - start;
 		System.out.println("[Binomio]mostrarBinomioTest: " + tiempo + " ms");
 	}
+	
+	@Test
+	public void queEvalueBienHornerGrado10(){
+		long start = System.nanoTime();
+		Assert.assertTrue(binomio.evaluarHorner(1)!=0);		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarHorner10: " + tiempo + " ns");
+	}	
+	
+	@Test
+	public void queEvalueBienHornerGrado20(){
+		long start = System.nanoTime();
+		Assert.assertTrue(binomio1.evaluarHorner(1)!=0);
+		long tiempo = System.nanoTime() - start; 
+		System.out.println("[BinomioEvaluarHorner20: " + tiempo + " ns");
+	}
+	
+	@Test
+	public void queEvalueBienHornerGrado30(){
+		long start = System.nanoTime();
+		Assert.assertTrue(binomio2.evaluarHorner(1) !=0);
+		long tiempo = System.nanoTime() - start; 
+		System.out.println("[BinomioEvaluarHorner30: " + tiempo + " ns");
+		System.out.println("");
+	}
+	
+	@Test
+	public void queEvalueBienPowGrado10(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio.evaluarPow(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarPowGrado10: " + tiempo + " ns");
+	}
+	
+	@Test
+	public void queEvalueBienPowGrado20(){
+		long start = System.nanoTime();
+		Assert.assertTrue(binomio1.evaluarPow(1)!=0);
+		long tiempo = System.nanoTime() - start; 
+		System.out.println("[BinomioEvaluarPowGrado20: " + tiempo + " ns");
+	}
+	
+	
+	@Test
+	public void queEvalueBienPowGrado30(){
+		long start = System.nanoTime();
+		Assert.assertTrue(binomio2.evaluarPow(1)!=0);
+		long tiempo = System.nanoTime() - start; 
+		System.out.println("[BinomioEvaluarPowGrado30: " + tiempo + " ns");
+		System.out.println("");
+	}
+	
+	@Test
+	public void queEvalueBienMSucesivasGrado10(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio.evaluarMSucesivas(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarMsucesivasGrado10: " + tiempo + " ns");
+	}
+	
+	@Test
+	public void queEvalueBienMSucesivasGrado20(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio1.evaluarMSucesivas(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarMsucesivasGrado20: " + tiempo + " ns");
+	}	
+
+	@Test
+	public void queEvalueBienMSucesivasGrado30(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio2.evaluarMSucesivas(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarMsucesivasGrado30: " + tiempo + " ns");
+		System.out.println("");
+	}	
+
+	@Test
+	public void queEvalueBienProgDinamicaGrado10(){
+		long start = System.nanoTime();
+		Assert.assertTrue(binomio.evaluarProgDinamica(1) !=0);		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarProgDinamica10: " + tiempo + " ns");
+	}	
+
+	@Test
+	public void queEvalueBienProgDinamicaGrado20(){
+		long start = System.nanoTime();
+		Assert.assertTrue(binomio1.evaluarProgDinamica(1) !=0);		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarProgDinamica20: " + tiempo + " ns");
+	}
+	
+	@Test
+	public void queEvalueBienProgDinamicaGrado30(){
+		long start = System.nanoTime();
+		Assert.assertTrue(binomio2.evaluarProgDinamica(1) !=0);		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarProgDinamica30: " + tiempo + " ns");
+		System.out.println("");
+	}
+	
+	@Test
+	public void queEvalueBienRecursivaParGrado10(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio.evaluarRecursivaPar(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarRecursivaPar10: " + tiempo + " ns");
+	}	
+	
+	@Test
+	public void queEvalueBienRecursivaParGrado20(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio1.evaluarRecursivaPar(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarRecursivaPar20: " + tiempo + " ns");
+	}	
+	
+	@Test
+	public void queEvalueBienRecursivaParGrado30(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio2.evaluarRecursivaPar(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarRecursivaPar30: " + tiempo + " ns");
+		System.out.println("");
+	}	
+	
+	@Test
+	public void queEvalueBienRecursivaGrado10(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio.evaluarRecursiva(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarRecursiva10: " + tiempo + " ns");
+	}	
+	
+	@Test
+	public void queEvalueBienRecursivaGrado20(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio1.evaluarRecursiva(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarRecursiva20: " + tiempo + " ns");
+	}	
+	
+	@Test
+	public void queEvalueBienRecursivaGrado30(){
+		long start = System.nanoTime();
+		
+		Assert.assertTrue(binomio2.evaluarRecursiva(1)!=0);
+		
+		long tiempo = System.nanoTime() - start;
+		System.out.println("[BinomioEvaluarRecursiva30: " + tiempo + " ns");
+		System.out.println("");
+	}	
 }
